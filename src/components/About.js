3cf4dc69e8c8ea16919006cdaab8aec714e9f675
Patricia <patriciamvbog@gmail.com>
2023-05-21 13:04:49 +0100

    add style to contact component, here has been used media queries

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,8 @@ function About() {
                <p className='intro__parraf__p'>I'm <span className='intro__parraf__p__name'>Patricia Montes de oca</span> Junior Full Stack developer.</p>
                <h4 className='intro__parraf__subtitle'>Let me introduce myself</h4>
                <p className='intro__parraf__p'>I'm a junior full stack developer, currently I'm looking for a new challenge where I can continue learning and improve my skills and knowledge. I have experience working within a scrum and agile metodologies.</p>
-               <p className='intro__parraf__p'> I have knowledge of <span className='intro__parraf__p__name'> HTML5, JavaScript, SASS, React, MySQL </span>and basic knowledge of APIs and back end programming in applications with Node JS, Express and SQL.</p>
-               <p className='intro__parraf__p'>I love traveling and test the local food, also I like to ride my bicycle. Take a look at my GitHub for all my projects and contact me! </p>
+               <p className='intro__parraf__p'> I have knowledge of <span className='intro__parraf__p__name'> HTML5, JavaScript, CSS/SASS, React, MySQL </span>and basic knowledge of APIs and backend programming in applications with Node.js, Express.js.</p>
+               <p className='intro__parraf__p'>I love traveling and test the local food, also I like to ride my bicycle. Take a look at my GitHub for all my projects. </p>
           
             </p>
             <img src={photo} alt="" className='intro__img'/>
